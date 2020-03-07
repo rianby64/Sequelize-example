176e1d9dd399fd13f5ba93835da161977e918851
Andres Rios <rianby64@gmail.com>
2020-03-07 11:58:24 +0300

    Explain what is going on here

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Sequelize-example
+
+Что здесь делать?
+
+```sh
+
+./run-postgresql.sh # для запуска БД и миграции...
+
+npm start # увидишь '[] here we go' или
+npm test  # увидишь тест
+```
--- run-postgresql.sh
+++ run-postgresql.sh
@@ -1,2 +1,9 @@
 docker build -t pg-example-10 -f postgres.Dockerfile . &&
 docker run -d --rm --name pg-example-10-container -p 5432:5432 pg-example-10
+
+sleep 2
+
+cd src
+npx sequelize db:migrate
+
+cd ..
